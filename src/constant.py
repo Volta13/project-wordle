bfc4937bd3afe0b6ab3ef121d3cc9bcb2c427062
cmoi-ugo <magister18yt@gmail.com>
2026-02-06 19:06:44 +0100

    add ansi terminal colors

--- src/constant.py
+++ src/constant.py
@@ -2,3 +2,9 @@
 WORD_LENGTH = 5
 MAX_ATTEMPTS = 6
 WORD_FILE = 'words.txt'
+
+# Terminal Colors
+COLOR_CORRECT = '\033[92m' # Green
+COLOR_PRESENT = '\033[93m' # Yellow
+COLOR_ABSENT = '\033[90m' # Gray
+COLOR_RESET = '\033[0m'
